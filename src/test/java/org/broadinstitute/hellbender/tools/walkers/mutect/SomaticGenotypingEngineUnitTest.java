6b4f10fdb5897c8887bb131f816dba88bf0916c6
David Benjamin <davidben@broadinstitute.org>
2017-01-26 09:23:21 -0500

    going to work with subsetted likelihood matrices

---
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/mutect/SomaticGenotypingEngineUnitTest.java
@@ -0,0 +1,10 @@
+package org.broadinstitute.hellbender.tools.walkers.mutect;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by David Benjamin on 1/26/17.
+ */
+public class SomaticGenotypingEngineUnitTest {
+
+}
